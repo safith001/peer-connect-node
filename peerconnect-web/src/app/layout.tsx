51741820ce1ac208cffcbba592834c3978872cf0
safith001 <safithmuslim@gmailcom>
2026-09-06 00:06:12 +0530

    feat: configure Google Analytics GA4 with measurement ID G-2V2H4T1GZC

--- peerconnect-web/src/app/layout.tsx
+++ peerconnect-web/src/app/layout.tsx
@@ -20,14 +20,41 @@ export const metadata: Metadata = {
 import { AuthProvider } from "@/context/AuthContext";
 import { NotificationProvider } from "@/context/NotificationContext";
 import NotificationToasts from "@/components/NotificationToasts";
+import Script from "next/script";
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID;
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        {/* Google Analytics 4 (GA4) Tag */}
+        {GA_MEASUREMENT_ID && (
+          <>
+            <Script
+              strategy="afterInteractive"
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+            />
+            <Script
+              id="google-analytics"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_MEASUREMENT_ID}', {
+                    page_path: window.location.pathname,
+                  });
+                `,
+              }}
+            />
+          </>
+        )}
+
         <AuthProvider>
           <NotificationProvider>
             <NotificationToasts />
